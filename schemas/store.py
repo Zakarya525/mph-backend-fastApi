077d2c168949e08df12572d8cb0aa4a96b5c613b
furqanalishah <syedfurqan@wanclouds.net>
2022-03-15 23:52:32 +0500

    add product list into StoreResponseSchema

--- schemas/store.py
+++ schemas/store.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+from schemas.product import ProductResponseSchema
+
 
 class CreateStoreSchema(BaseModel):
     title: str
@@ -12,6 +14,7 @@ class StoreResponseSchema(BaseModel):
     id: str
     title: str
     description: Optional[str]
+    products: [ProductResponseSchema]
 
 
 class StoreUpdateSchema(BaseModel):
